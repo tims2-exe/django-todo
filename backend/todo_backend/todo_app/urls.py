cd7c75320691a199fdc5729c9985ef73c8840fa1
tims-exe <timonkjohn@gmail.com>
2024-11-04 21:12:55 +0530

    todo serializer

--- backend/todo_backend/todo_app/urls.py
+++ backend/todo_backend/todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test)
+    path('', views.todo_list, name= 'todo_list'),
+    path('new/', views.add_todo, name = 'new_todo'),
 ]
--- backend/todo_backend/todo_app/views.py
+++ backend/todo_backend/todo_app/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 
 def test(request):
     return HttpResponse('hello world !!')
+
+def todo_list(request):
+    tasks = Todo.objects.all()
+    return HttpResponse('test')
+
+def add_todo(request):
+    Todo.objects.create(task = 'test', completed = False)
+    return redirect('todo_list')
+
