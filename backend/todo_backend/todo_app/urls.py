fcb7e857072390afb970a079be6626977d2bd0a1
tims-exe <timonkjohn@gmail.com>
2024-11-06 22:24:51 +0530

    backend done

--- backend/todo_backend/todo_app/urls.py
+++ backend/todo_backend/todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list_view_create, name = 'todo_list'),
+    path('tasks/', views.todo_list_view_create, name = 'todo_list'),
+    path('tasks/<int:pk>/', views.todo_update_delete_view, name='todo_edit')
 ]
--- backend/todo_backend/todo_backend/settings.py
+++ backend/todo_backend/todo_backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +50,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+    "http://127.0.0.1:5500",  
 ]
 
 ROOT_URLCONF = 'todo_backend.urls'
