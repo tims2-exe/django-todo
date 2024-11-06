1c3d1fcea0080cdd9d702ab7adb766bdb9fc62b0
tims-exe <timonkjohn@gmail.com>
2024-11-06 22:22:28 +0530

    todo functionalities done

--- todo/src/components/Tasks.jsx
+++ todo/src/components/Tasks.jsx
@@ -1,37 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import del from '../assets/trash.png';
 import edit from '../assets/edit.png';
 import check from '../assets/checkbox.png';
 import uncheck from '../assets/unchecked.png';
+import update from '../assets/add.png';
+import axios from 'axios';
 
-const Tasks = () => {
-  const [checked, setChecked] = useState(false);
+const Tasks = ({ id, name, status, fetchTodos }) => {
+  const [checked, setChecked] = useState(status); 
+  const [editMode, setEditMode] = useState(false);
+  const [updatedTask, setUpdatedTask] = useState(name);
+  
+  const inputRef = useRef(null);
 
   const toggleChecked = () => {
-    setChecked(prevState => !prevState)
+    setChecked(prevState => {
+      const newChecked = !prevState;
+      handleUpdate(newChecked);
+      return newChecked;
+    });
+  }
+
+  const handleEdit = () => {
+    setEditMode(true);
+  }
+
+  const handleUpdate = (completedStatus = checked) => {
+    if (updatedTask !== name || completedStatus !== status) {
+      axios.put(`http://127.0.0.1:8000/tasks/${id}/`, { task: updatedTask, completed: completedStatus })
+        .then(() => {
+          setEditMode(false);
+          fetchTodos();
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      setEditMode(false);
+    }
+  }
+
+  const deleteTodo = () => {
+    axios.delete(`http://127.0.0.1:8000/tasks/${id}/`)
+      .then(() => {
+        fetchTodos();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  React.useEffect(() => {
+    if (editMode && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editMode]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleUpdate();
+    }
   }
 
   return (
     <div className='my-7 flex flex-row justify-between'>
-        <div className='text-[20px] flex flex-row justify-center items-start'>
-            <button onClick={toggleChecked} className='w-8 mr-5 opacity-80'>
-                <img src={checked ? check : uncheck} alt="" />
-            </button>
-            <p>Tasks</p>
-        </div>
-        <div className='w-[80px] flex flex-row opacity-80'>
-            <button>
-                <img src={edit} alt="" />
-            </button>
-            <div className='w-20'>
-
-            </div>
-            <button>
-                <img src={del} alt="" />
-            </button>
-        </div>
+      <div className='text-[20px] flex flex-row justify-center items-start'>
+        <button onClick={toggleChecked} className='w-8 mr-5 opacity-80'>
+          <img src={checked ? check : uncheck} alt="" />
+        </button>
+        {editMode ? (
+          <input
+            type="text"
+            value={updatedTask}
+            onChange={(e) => setUpdatedTask(e.target.value)}
+            className="bg-transparent text-white caret-white"
+            ref={inputRef} 
+            onKeyDown={handleKeyDown} 
+          />
+        ) : (
+          <p>{name}</p>
+        )}
+      </div>
+      <div className='w-[80px] flex flex-row opacity-80'>
+        {editMode ? (
+          <button onClick={handleUpdate}>
+            <img src={update} alt="Save" className='w-[60px]' />
+          </button>
+        ) : (
+          <button onClick={handleEdit}>
+            <img src={edit} alt="Edit" />
+          </button>
+        )}
+        <div className='w-20'></div>
+        <button onClick={() => deleteTodo()}>
+          <img src={del} alt="Delete" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Tasks
+export default Tasks;
--- todo/src/pages/HomePage.jsx
+++ todo/src/pages/HomePage.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import icon from '../assets/add.png'
 import Tasks from '../components/Tasks'
 import './styles/homepagestyles.css'
+import axios from 'axios';
 
 const HomePage = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = () => {
+    axios.get('http://127.0.0.1:8000/tasks/')
+      .then((response) => setTodos(response.data.reverse()))
+      .catch((error) => console.log('ERROR : ', error));
+  }
+
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value)
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+        addNewTodo();
+    }
+  };
+
+  const addNewTodo = () => {
+    if (newTodo) {
+        axios.post('http://127.0.0.1:8000/tasks/', {task : newTodo, completed : false})
+        .then((response) => {
+            console.log('Todo Added : ', response.data);
+            setNewTodo('');
+            fetchTodos();
+        })
+        .catch((error) => {
+            console.log('ERROR : ', error);
+        })
+    } 
+  }
+
   return (
     <div className='bg-gradient-to-br from-purple-800 to-black h-screen flex flex-col justify-center items-center'>
         <div className='text-white w-[40vw] h-[65vh] px-10 py-5'>
@@ -11,14 +49,14 @@ const HomePage = () => {
                 Todo
             </div>
             <div className='h-full overflow-auto custom-scroll'>
-                <Tasks />
-                <Tasks />
-                <Tasks />
+                {todos.map((todo) => (
+                    <Tasks key={todo.id} id = {todo.id} name={todo.task} status={todo.completed} fetchTodos={fetchTodos}/>
+                ))}
             </div>
         </div>
         <div className='border-2 border-white opacity-60 rounded-[20px] w-[40vw] h-[8vh] mt-10 bg-transparent px-5 flex flex-row'>
-            <input type="text" placeholder='New Task' className='bg-transparent text-white caret-white h-full w-full border-none px-5 outline-none' />
-            <button className='w-8 h-full'>
+            <input type="text" value={newTodo} placeholder='New Task' onChange={handleInputChange} onKeyDown={handleKeyPress} className='bg-transparent text-white caret-white h-full w-full border-none px-5 outline-none' />
+            <button className='w-8 h-full' onClick={addNewTodo}>
                 <img src={icon} alt="" />
             </button>
         </div>
